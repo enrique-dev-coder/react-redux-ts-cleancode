8f8dc71a706de653019692e506def24a17b7f1fa
enrique95 <enrique_alvarado95@hotmail.com>
2022-09-15 13:24:37 -0500

    table with data and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+//gracias a los alias se simplifican las rutas importadas
 import './App.css';
+import { Navbar } from './components';
+import { Home } from './pages/Home';
+import { LayoutContainer } from './styled-components';
 
 function App() {
-  return <div className="App">HOLA MUNDO</div>;
+  return (
+    <>
+      <Navbar />
+      <LayoutContainer>
+        <Home />
+      </LayoutContainer>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+//para esto es el alias
+import { People } from '@/data/people';
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	return <div >Home</div>;
+  const pageSize = 5;
+  //cada objeto representa una de las columnas
+  //porrows la tabla toma al objeto people
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      //pones el value de cada field, sin necesidad de maps ni nada de eso
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'category',
+      headerName: 'Categories',
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+  return (
+    <DataGrid
+      rows={People}
+      columns={columns}
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      pageSize={pageSize}
+      rowsPerPageOptions={[pageSize]}
+      // el id asociado como un key
+      getRowId={(row: any) => row.id}
+    />
+  );
 };
 
 export default Home;
-
